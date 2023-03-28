482d20fbb56efa8e44104847c8b05c01352f8e52
Pen <502498669@qq.com>
2023-03-28 22:06:50 +0800

    FOR RUN

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from training import Trainer
 # Generate datasets from text file
 path = "data"
 N = 128
-config = read_config(N,path)
+config = read_config(N, path)
 train_dataset, valid_dataset = get_datasets(config)
 checkpoint_path = "."
 
@@ -18,13 +18,12 @@ num_epochs = 10
 trainer = Trainer(model, config, lr=0.003)
 trainer.load_checkpoint(checkpoint_path)
 
-for epoch in range(num_epochs):
-	print("========= Epoch %d of %d =========" % (epoch+1, num_epochs))
-	train_loss = trainer.train(train_dataset)
-	valid_loss = trainer.test(valid_dataset)
-	trainer.save_checkpoint(epoch, checkpoint_path)
+if __name__ == '__main__':
+    for epoch in range(num_epochs):
+        print("========= Epoch %d of %d =========" % (epoch + 1, num_epochs))
+        train_loss = trainer.train(train_dataset)
+        valid_loss = trainer.test(valid_dataset)
+        trainer.save_checkpoint(epoch, checkpoint_path)
 
-	print("========= Results: epoch %d of %d =========" % (epoch+1, num_epochs))
-	print("train loss: %.2f| valid loss: %.2f\n" % (train_loss, valid_loss) )
-
-
+        print("========= Results: epoch %d of %d =========" % (epoch + 1, num_epochs))
+        print("train loss: %.2f| valid loss: %.2f\n" % (train_loss, valid_loss))
